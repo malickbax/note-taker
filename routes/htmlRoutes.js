5cd72611a6ad933c26deae25103a4aaaac0f4f56
malickbax <malickbax@gmail.com>
2022-07-30 10:40:31 -0400

    Adding htmlRoutes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+// Calling required files
+const path = require("path");
+const router = require("express").Router();
+
+router.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+router.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// Export file
+module.exports = router;
